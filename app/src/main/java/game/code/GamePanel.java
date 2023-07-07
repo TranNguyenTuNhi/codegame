af717967debd189ec1da3d8c164954bb0b352faa
xuandd <103272537+xuanchimto@users.noreply.github.com>
2023-07-07 17:26:54 +0700

    update

--- app/src/main/java/game/code/GamePanel.java
+++ app/src/main/java/game/code/GamePanel.java
@@ -2,14 +2,40 @@ package game.code;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class GamePanel extends SurfaceView {
-    public GamePanel(Context context){
+import androidx.annotation.NonNull;
+
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
+
+    public GamePanel(Context context) {
         super(context);
+        // TODO Auto-generated constructor stub
+        getHolder().addCallback(this);
+        setFocusable(true);
     }
+
+
     @Override
-    protected void onDraw(Canvas canvas){
+    protected void onDraw(Canvas canvas) {
+        // TODO Auto-generated method stub
         super.onDraw(canvas);
     }
+
+    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void surfaceCreated(SurfaceHolder arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void surfaceDestroyed(SurfaceHolder arg0) {
+        // TODO Auto-generated method stub
+
+    }
 }
+
