39f04661086b8a313323c0270819ab32222d0acc
xuandd <103272537+xuanchimto@users.noreply.github.com>
2023-07-07 17:52:24 +0700

    update panel

--- app/src/main/java/game/code/GamePanel.java
+++ app/src/main/java/game/code/GamePanel.java
@@ -1,41 +1,66 @@
 package game.code;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-import androidx.annotation.NonNull;
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
-public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
+    private MainThread thread;
+    private Bitmap bitmap;
+    private int mX;
+    private int mY;
 
     public GamePanel(Context context) {
         super(context);
-        // TODO Auto-generated constructor stub
         getHolder().addCallback(this);
+        thread = new MainThread(getHolder(), this);
         setFocusable(true);
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_background);
     }
-
 
     @Override
     protected void onDraw(Canvas canvas) {
-        // TODO Auto-generated method stub
         super.onDraw(canvas);
-    }
-
-    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
-        // TODO Auto-generated method stub
+        canvas.drawColor(Color.BLACK);
+        canvas.drawBitmap(bitmap, 10, 10, null);
 
     }
 
-    public void surfaceCreated(SurfaceHolder arg0) {
-        // TODO Auto-generated method stub
-
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mX = (int) event.getX() - bitmap.getWidth() / 2;
+        mY = (int) event.getY() - bitmap.getHeight() / 2;
+        return true;
     }
 
-    public void surfaceDestroyed(SurfaceHolder arg0) {
-        // TODO Auto-generated method stub
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // Handle surface changes if needed
+    }
 
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while (retry) {
+            try {
+                thread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
-
