b0364f15e4c8b9f44c45bdaf709203ae71690027
Trần Nguyễn Tú Nhi <tunhi1042@gmail.com>
2023-07-07 17:27:20 +0700

    Trong hàm run ta tạo ra biến đếm và trong vòng lặp while ta cho biến đếm tăng lên rồi dùng lệnh Log.d để xuất ra Logcat.

--- app/src/main/java/game/code/MainThread.java
+++ app/src/main/java/game/code/MainThread.java
@@ -1,5 +1,7 @@
 package game.code;
 
+import android.util.Log;
+
 public class MainThread extends Thread{
     private boolean running;
     public void setRunning(boolean r){
@@ -8,11 +10,14 @@ public class MainThread extends Thread{
     @Override
     public void run(){
         //
-        super.run();
 
+        super.run();
+        long dem=0L;
         while (running){
+            dem++;
             //cap nhat lai trang thai game
             // render du lieu ra man hinh
+            Log.d("testloop:", "loop "+ dem);
         }
     }
 }
