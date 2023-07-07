a9b5ba24d14fd5893271c08dd7e0629cf69ba35e
Trần Nguyễn Tú Nhi <tunhi1042@gmail.com>
2023-07-07 17:38:42 +0700

    Them 2 bien vao ham MainThread

--- app/src/main/java/game/code/MainThread.java
+++ app/src/main/java/game/code/MainThread.java
@@ -1,16 +1,25 @@
 package game.code;
 
 import android.util.Log;
+import android.view.SurfaceHolder;
 
 public class MainThread extends Thread{
+    //them 2 bien
+    private SurfaceHolder surfaceholder;
+    private GamePanel gamepanel;
     private boolean running;
+
+    //them ham
+    public MainThread(SurfaceHolder surfaceholder, GamePanel gamepanel){
+        this.surfaceholder = surfaceholder;
+        this.gamepanel = gamepanel;
+    }
     public void setRunning(boolean r){
         running = r;
     }
     @Override
     public void run(){
-        //
-
+        // TODO Auto-generated method stub
         super.run();
         long dem=0L;
         while (running){
